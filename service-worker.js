b75a5a8f47ecd332e6b76b28f985d7f6d41b7d39
Eslam Ahmed <76213989+EslamAhmedGaber@users.noreply.github.com>
2026-06-14 07:13:20 +0300

    Align UI chrome with premium reference

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "elite-igcse-kill-v139";
+const CACHE_VERSION = "elite-igcse-kill-v140";
 
 self.addEventListener("install", (event) => {
   event.waitUntil(self.skipWaiting());
